bb93283a0bee549043b4c3a29da1e0ba702500e0
maguowei <imaguowei@gmail.com>
2019-09-26 14:15:45 +0800

    crawling follower

--- app/event.py
+++ app/event.py
@@ -23,42 +23,34 @@ class Events:
         # 推荐
         if '/aweme/v1/feed/' in url:
             data = json.loads(content)
-            # pprint(data)
             if data['status_code'] == 0 and data['has_more'] == 1:
-                # db['nearby'].insert_one(data)
-                # db['nearby_videos'].insert_many(data['aweme_list'])
                 for aweme in data['aweme_list']:
                     aweme_id = aweme['aweme_id']
                     db['feed_videos'].replace_one({'aweme_id': aweme_id}, aweme, upsert=True)
                     redis_client.sadd('users', aweme['author_user_id'])
-
         # 同城
         if '/aweme/v1/nearby/feed/' in url:
             data = json.loads(content)
-            # pprint(data)
             if data['status_code'] == 0 and data['has_more'] == 1:
-                # db['nearby'].insert_one(data)
-                # db['nearby_videos'].insert_many(data['aweme_list'])
                 for aweme in data['aweme_list']:
                     aweme_id = aweme['aweme_id']
                     db['nearby_videos'].replace_one({'aweme_id': aweme_id}, aweme, upsert=True)
                     redis_client.sadd('users', aweme['author_user_id'])
-
         # 关注
         elif '/v2/follow/feed/' in url:
             data = json.loads(content)
-            # pprint(data)
-            db['follow'].insert_one(data)
+            if data['status_code'] == 0 and data['has_more'] == 1:
+                for aweme in data['data']:
+                    aweme_id = aweme['aweme']['aweme_id']
+                    db['follow_videos'].replace_one({'aweme.aweme_id': aweme_id}, aweme, upsert=True)
+                    redis_client.sadd('users', aweme['author_user_id'])
 
         # 关注列表
         elif '/aweme/v1/user/follower/list/' in url:
             data = json.loads(content)
-            db['followers'].insert_one(data)
-            followers = data['followers']
-            for follower in followers:
-                user = {
-                    'uid': follower['uid'],
-                    'nickname': follower['nickname'],
-                }
-                redis_client.sadd('users', user['uid'])
-                # print(user)
+            if data['status_code'] == 0 and data['has_more'] == 1:
+                for follower in data['followers']:
+                    user_id = follower['uid']
+                    # nickname = follower['nickname'],
+                    db['follower_users'].replace_one({'uid': user_id}, follower, upsert=True)
+                    redis_client.sadd('users', user_id)
